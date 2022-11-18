3205ccc94ffe4ef79405f11b3e70ae58bb16b69c
linguoyan <linguoyan@vv.cn>
2022-11-18 16:09:34 +0800

    feat: 添加顶部栏组件

--- src/renderer/Component/BarTop.vue
+++ src/renderer/Component/BarTop.vue
@@ -1,37 +1,5 @@
-<script setup lang="ts">
-import { onMounted, ref, onUnmounted } from "vue";
-import { ipcRenderer } from "electron";
-defineProps<{ title?: string }>();
-let isMaximized = ref(false);
-let closeWindow = () => {
-    ipcRenderer.invoke("closeWindow");
-};
-let maxmizeMainWin = () => {
-    ipcRenderer.invoke("maxmizeWindow");
-};
-let minimizeMainWindow = () => {
-    ipcRenderer.invoke("minimizeWindow");
-};
-let unmaximizeMainWindow = () => {
-    ipcRenderer.invoke("unmaximizeWindow");
-};
-let winMaximizeEvent = () => {
-    isMaximized.value = true;
-};
-let winUnmaximizeEvent = () => {
-    isMaximized.value = false;
-};
-onMounted(() => {
-    ipcRenderer.on("windowMaximized", winMaximizeEvent);
-    ipcRenderer.on("windowUnmaximized", winUnmaximizeEvent);
-});
-onUnmounted(() => {
-    ipcRenderer.off("windowMaximized", winMaximizeEvent);
-    ipcRenderer.off("windowUnmaximized", winUnmaximizeEvent);
-});
-</script>
 <template>
-    <div class="topBar">
+    <div class="topB ar">
         <div class="winTitle">{{ title }}</div>
         <div class="winTool">
             <div @click="minimizeMainWindow">
@@ -49,12 +17,55 @@ onUnmounted(() => {
         </div>
     </div>
 </template>
+
+<script setup lang="ts">
+import { onMounted, ref, onUnmounted } from "vue";
+import { ipcRenderer } from "electron";
+defineProps<{ title?: string }>();
+
+let isMaximized = ref(false);
+//关闭窗口
+let closeWindow = () => {
+    ipcRenderer.invoke("closeWindow");
+};
+//最大化窗口
+let maxmizeMainWin = () => {
+    ipcRenderer.invoke("maxmizeWindow");
+};
+//最小化窗口
+let minimizeMainWindow = () => {
+    ipcRenderer.invoke("minimizeWindow");
+};
+//还原窗口
+let unmaximizeMainWindow = () => {
+    ipcRenderer.invoke("unmaximizeWindow");
+};
+//窗口最大化事件
+let winMaximizeEvent = () => {
+    isMaximized.value = true;
+};
+//窗口取消最大化事件
+let winUnmaximizeEvent = () => {
+    isMaximized.value = false;
+};
+onMounted(() => {
+    // 窗口最大化（或还原）不一定是通过点击最大化按钮触发的，也可能通过双击标题栏可拖拽区域触发的
+    // 所以在这里我们通过 ipcRenderer.on 来监听窗口的最大化或还原事件
+    ipcRenderer.on("windowMaximized", winMaximizeEvent);
+    ipcRenderer.on("windowUnmaximized", winUnmaximizeEvent);
+});
+onUnmounted(() => {
+    ipcRenderer.off("windowMaximized", winMaximizeEvent);
+    ipcRenderer.off("windowUnmaximized", winUnmaximizeEvent);
+});
+</script>
+
 <style scoped lang="scss">
 .topBar {
     display: flex;
     height: 25px;
     line-height: 25px;
-    -webkit-app-region: drag;
+    -webkit-app-region: drag; /* 可拖拽区域 */
     width: 100%;
 }
 .winTitle {
@@ -66,7 +77,7 @@ onUnmounted(() => {
 .winTool {
     height: 100%;
     display: flex;
-    -webkit-app-region: no-drag;
+    -webkit-app-region: no-drag; /* 可拖拽区域内的不可拖拽区域 */
 }
 .winTool div {
     height: 100%;
