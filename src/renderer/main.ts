e5e1db01952667724eeed4b66c6ea62624b80809
linguoyan <linguoyan687@163.com>
2022-11-23 11:37:58 +0800

    feat: 引入pinia状态管理

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -1,9 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./router";
+import { createPinia } from "pinia";
 
 import "./assets/style.css";
 //全局导入字体图标
 import "./assets/icon/iconfont.css";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(createPinia()).use(router).mount("#app");
