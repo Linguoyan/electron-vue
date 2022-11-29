61660c5054765131e026a9ee02beed4e682059ff
linguoyan <linguoyan687@163.com>
2022-11-29 14:44:58 +0800

    refactor: add remark

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -10,8 +10,9 @@ import "./assets/icon/iconfont.css";
 
 createApp(App).use(createPinia()).use(router).mount("#app");
 
-db("Chat")
-    .first()
-    .then((obj) => {
-        console.log(obj);
-    });
+// 测试：输出数据库的第一条数据
+// db("Chat")
+//     .first()
+//     .then((obj) => {
+//         console.log(obj);
+//     });
