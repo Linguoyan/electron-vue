86ac764e078d504cba9fde247356706506aa2664
linguoyan <linguoyan687@163.com>
2022-11-29 14:43:57 +0800

    test: 测试数据库数据输出

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -2,9 +2,16 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./router";
 import { createPinia } from "pinia";
+import { db } from "../common/db";
 
 import "./assets/style.css";
 //全局导入字体图标
 import "./assets/icon/iconfont.css";
 
 createApp(App).use(createPinia()).use(router).mount("#app");
+
+db("Chat")
+    .first()
+    .then((obj) => {
+        console.log(obj);
+    });
